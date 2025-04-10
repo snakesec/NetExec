3f4b22f977257571eeed0ec5c7a76357432956fd
Weidsom Nascimento <source@snakesecurity.org>
2025-04-10 11:58:24 -0700

    Force use of dedicated virtual environment

--- AFOSBUILD.sh
+++ AFOSBUILD.sh
@@ -9,7 +9,7 @@ python3 -m venv /opt/ANDRAX/netexec
 
 source /opt/ANDRAX/netexec/bin/activate
 
-/opt/ANDRAX/netexec/bin/pip install pyinstaller .
+/opt/ANDRAX/netexec/bin/pip install .
 
 if [ $? -eq 0 ]
 then
@@ -20,17 +20,4 @@ else
   exit 1
 fi
 
-/opt/ANDRAX/netexec/bin/pyinstaller netexec.spec
-
-if [ $? -eq 0 ]
-then
-  # Result is OK! Just continue...
-  echo "Pyinstaller BUILD... PASS!"
-else
-  # houston we have a problem
-  exit 1
-fi
-
-cp -Rf dist/nxc /opt/ANDRAX/bin/
-
-rm -rf /opt/ANDRAX/netexec/
+cp -Rf andraxbin/* /opt/ANDRAX/bin
